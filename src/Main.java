0a348a4818a7f1e7d2318b761cab2b447fb1146a
Maksim <raymax.spb@gmail.com>
2024-03-05 20:19:48 +0300

    Домашнее задание 8

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Task 1");
+        int [] array = new int[3];
+        array [0] = 1;
+        array [1] = 2;
+        array [2] = 3;
+
+        System.out.println("Task 2");
+        double [ ] array1 = {1.57, 7.654, 9.986};
+
+        System.out.println("Task 3");
+        double []
     }
 }
